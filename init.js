8a034f4ff21f7f35a938d16037364ed4cfad7cd9
Ishaan Mahesh <78961353+ishaan5199@users.noreply.github.com>
2021-10-27 08:44:49 +0530

    api objects

--- init.js
+++ init.js
@@ -22,19 +22,19 @@ var arrayData;
 const urlSearchParams = new URLSearchParams(window.location.search);
 
 const params = Object.fromEntries(urlSearchParams.entries());
-//console.log(params["val"]);
+console.log(params["val"]);
 
 const docSheet = "https://opensheet.vercel.app/" + params["val"] + "/Sheet1"
 fetch(docSheet).then(res => res.json()).then(data => {
-    arrayData = data.map(({Position, Name, Institute}) => ([Position, Name, Institute]))
+    arrayData = data;
 }).then(() => {
     $(document).ready(function(){
         $('#table').DataTable( {
             data: arrayData,
             columns:[
-                {title : "Position"},
-                {title : "Name"},
-                {title : "Institute"}
+                {arrayData : "Position"},
+                {arrayData : "Name"},
+                {arrayData : "Institute"}
             ]
         } );
     })
