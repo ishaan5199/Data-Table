f55db5a574398d3977509833116421e84bbf0926
Ishaan Mahesh <78961353+ishaan5199@users.noreply.github.com>
2021-10-27 08:37:32 +0530

    dynamic api link

--- init.js
+++ init.js
@@ -20,10 +20,12 @@
 
 var arrayData;
 const urlSearchParams = new URLSearchParams(window.location.search);
-console.log(urlSearchParams);
-const params = Object.fromEntries(urlSearchParams.entries());
 
-fetch('https://opensheet.vercel.app/1OzpHafZbyBR4HhL0AFzZCxE9wLFvQt5KVvifp2PrxIE/Sheet1').then(res => res.json()).then(data => {
+const params = Object.fromEntries(urlSearchParams.entries());
+//console.log(params["val"]);
+
+const docSheet = "https://opensheet.vercel.app/" + params["val"] + "/Sheet1"
+fetch(docSheet).then(res => res.json()).then(data => {
     arrayData = data.map(({Position, Name, Institute}) => ([Position, Name, Institute]))
 }).then(() => {
     $(document).ready(function(){
