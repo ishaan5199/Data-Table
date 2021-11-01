c73329db2733597d1ad98cdfd69d31844a8a5dc3
Ishaan Mahesh <78961353+ishaan5199@users.noreply.github.com>
2021-11-01 19:38:14 +0530

    Update init.js

--- init.js
+++ init.js
@@ -5,7 +5,7 @@ const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
 console.log(params["val"]);
 
-const docSheet = "https://opensheet.vercel.app/" + params["val"] + "/Sheet1"
+const docSheet = "https://opensheet.vercel.app/" + /* params["val"] */"1JKhyzO4i2Y0qScSVORNXUyLWmAM1eqVXeeJOdvpzp00" + "/Sheet1"
 fetch(docSheet).then(res => res.json()).then(data => {
     arrayData = data;
     keyData = Object.keys(arrayData[0]);
@@ -13,6 +13,13 @@ fetch(docSheet).then(res => res.json()).then(data => {
     keyData.forEach(element => {
         columnData.push({"data" : element, "title" : element})
     });
+
+    if(arrayData[0]["Team Members"] != ""){
+        for (let index = 0; index < arrayData.length; index++) {
+            arrayData[index]["Team Members"] = arrayData[index]["Team Members"].split(",").join("<br/>");
+            arrayData[index]["Institute"] = arrayData[index]["Institute"].split(",").join("<br/>");
+        }
+    }
 
 }).then(() => {
     $(document).ready(function(){
@@ -24,6 +31,10 @@ fetch(docSheet).then(res => res.json()).then(data => {
             ],
             "dom":'<"top"if><"table"t><"bottom"p>',
             lengthChange: false,
+            searching: false,
+            paging: false,
+            info: false,
+            ordering: false
         } );
     })
 })
