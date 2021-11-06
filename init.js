b8293eecb5cfe11a2ec3f3cc7968826a06d62659
Ishaan Mahesh <78961353+ishaan5199@users.noreply.github.com>
2021-11-06 19:09:42 +0530

    Update init.js

--- init.js
+++ init.js
@@ -5,6 +5,7 @@ const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
 
 const docSheet = "https://opensheet.vercel.app/" + params["val"] + "/Sheet1"
+
 fetch(docSheet).then(res => res.json()).then(data => {
     arrayData = data;
     keyData = Object.keys(arrayData[0]);
@@ -15,8 +16,8 @@ fetch(docSheet).then(res => res.json()).then(data => {
 
     if(Object.keys(arrayData[0]).length != 3){
         for (let index = 0; index < arrayData.length; index++) {
-            arrayData[index]["Team Members"] = arrayData[index]["Team Members"].split(",").join("<br/><hr/>");
-            arrayData[index]["Institute"] = arrayData[index]["Institute"].split(",").join("<br/><hr/>");
+            arrayData[index][Object.keys(arrayData[0])[2]] = arrayData[index][Object.keys(arrayData[0])[2]].split(",").join("<br/><hr/>");
+            arrayData[index][Object.keys(arrayData[0])[3]] = arrayData[index][Object.keys(arrayData[0])[3]].split(",").join("<br/><hr/>");
         }
     }
 
